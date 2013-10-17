64e3206182b4aebf5d76a57c7826a2230f3f67f3
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-10-17 14:17:02 +0200

    initial commit of the Google Content API for Shopping gem

---
+++ lib/google/content/shopping.rb
@@ -0,0 +1,9 @@
+require "google/content/shopping/version"
+
+module Google
+  module Content
+    module Shopping
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/google/content/shopping/version.rb
@@ -0,0 +1,7 @@
+module Google
+  module Content
+    module Shopping
+      VERSION = "0.0.1"
+    end
+  end
+end
