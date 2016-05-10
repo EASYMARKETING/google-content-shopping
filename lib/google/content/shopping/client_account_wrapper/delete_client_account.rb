b49807114988330d3b78e458b162e205b5145e6f
Christian Semmler <mail@csemmler.com>
2016-05-10 14:17:23 +0200

    update API

--- lib/google/content/shopping/client_account_wrapper/delete_client_account.rb
+++ lib/google/content/shopping/client_account_wrapper/delete_client_account.rb
@@ -8,7 +8,7 @@ module Google
         # feeds will be removed from the Google search index in approximately
         # one day.
         def perform
-          response = self.class.delete("/content/v1/#{parent_account_number}/managedaccounts/#{client_account_number}",
+          response = self.class.delete("/content/v2/#{parent_account_number}/accounts/#{client_account_number}",
                                        standard_header)
 
           response.code == 200
